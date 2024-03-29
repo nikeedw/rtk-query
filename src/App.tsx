6888ce1f30ed29fa05a0dcdee17a614d91bbd31e
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-03-29 16:49:02 +0200

    Added some methods using UserSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import PostContainer from "./components/PostContainer";
-import PostContainer2 from "./components/PostContainer2";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { fetchUsers, addUser, removeUserById } from "./store/reducers/ActionCreators";
+import { IUser } from "./models/IUser";
 
 function App() {
+	const dispatch = useAppDispatch();
+	const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, [])
+
+	const handleAddUser = () => {
+		const name = prompt();
+		dispatch(addUser({name, email: name} as IUser));
+	}
+
+	const handleDeleteUser = (user: IUser) => {
+		dispatch(removeUserById(user))
+	}
+
+	if(isLoading) {
+		return <h1>Loading...</h1>
+	}
+
+	if(error) {
+		return <h1>{error}</h1>
+	}
 
 	return (
-		<div className="App">
-			<PostContainer />
-			<PostContainer2 />
+		<div className="Application">
+			<button onClick={handleAddUser}>Add User</button>
+			{users.map(user =>
+				<div key={user.id} className="item">
+					{user.id} - {user.name}
+					<button onClick={() => handleDeleteUser(user)}>Delete</button>
+				</div>	
+			)}
 		</div>
 	);
 }
